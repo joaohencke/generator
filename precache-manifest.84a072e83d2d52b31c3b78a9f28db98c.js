f6516f4dd1cc62fb9b05805b87c8323c43f71d74
Joao Hencke <joao.hencke@gmail.com>
2018-12-02 20:29:55 -0200

    Updates

---
+++ precache-manifest.84a072e83d2d52b31c3b78a9f28db98c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3046095595ec3aae1148",
+    "url": "/generator/static/js/runtime~main.30460955.js"
+  },
+  {
+    "revision": "999eeb9b74b2702745dc",
+    "url": "/generator/static/js/main.999eeb9b.chunk.js"
+  },
+  {
+    "revision": "2ac5da0f5e4ac04d7df1",
+    "url": "/generator/static/js/1.2ac5da0f.chunk.js"
+  },
+  {
+    "revision": "2ac5da0f5e4ac04d7df1",
+    "url": "/generator/static/css/1.e31a8290.chunk.css"
+  },
+  {
+    "revision": "419aeb9b97186829ab6ce58787211de6",
+    "url": "/generator/index.html"
+  }
+];
